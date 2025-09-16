84d6bf37ebd59f79b255d47022eeb1077360aa15
Harsh Tiwari <harshtiwari343@email.com>
2025-09-16 14:46:14 +0530

    Added db.test.js

---
+++ __test__/db.test.js
@@ -0,0 +1,15 @@
+const { findUser, saveUser } = require("./testing_example/db");
+
+describe('DB module direct tests', () => {
+    test('should save and find user', () => {
+        const newUser = { username: "sam", password: "xyz" };
+        saveUser(newUser);
+        const found = findUser("sam");
+        expect(found).toEqual(newUser);
+    });
+
+    test('should return undefined for non existing user', () => {
+        const found = findUser("ghost");
+        expect(found).toBeUndefined();
+    });
+});
